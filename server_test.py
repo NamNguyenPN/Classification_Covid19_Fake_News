1ac02e963f371d7a4e7c444e3968a9b992899cda
fairypham <namnd.tth@gmail.com>
2021-05-25 01:57:16 +0700

    update html

--- server_test.py
+++ server_test.py
@@ -2,11 +2,14 @@ import backend # Import the python file containing the ML model
 from flask import Flask, request, render_template,jsonify # Import flask libraries
 
 
-app = Flask(__name__,template_folder="templates")
+app = Flask(__name__,template_folder="templates",static_url_path='/static')
 
 @app.route("/")
 def home():
-    # return "Hello, World!"
+    return render_template('home1.html')   
+
+@app.route("/")
+def retry():
     return render_template('home1.html')   
 
 @app.route("/classify",methods=['POST','GET'])
@@ -16,10 +19,20 @@ def classify_type():
         exam = request.args.get('txt') # Get parameters for sentence
 
         # Get the output from the classification model
-        variety = backend.classify(exam)
+        label = backend.classify(exam)
+        if exam == "":
+            img = "error.png"
+            label_class = "input alert-warning"
+            return render_template('home1.html', label='Nothing to predict',sentence='', label_class=label_class, image = img, display="block")
+        if label == "fake":
+            img = "sad.png"
+            label_class = "input alert-danger"
+        else:
+            img = "happy.png"
+            label_class = "input alert-success"
 
         # Render the output in new HTML page
-        return render_template('home1.html', variety=variety,sentence=exam)
+        return render_template('home1.html', label=label,sentence=exam, label_class=label_class, image = img, display="block")
     except:
         return 'Error'
 
